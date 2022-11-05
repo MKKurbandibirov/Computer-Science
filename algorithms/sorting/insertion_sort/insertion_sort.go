01cbecdfbd5c1cd5eea744ced41e272e579146f0
MKKurbandibirov <MKKurbandibirov@yandex.ru>
2022-11-05 19:42:22 +0300

    Complete Insertion SOrt

---
+++ algorithms/sorting/insertion_sort/insertion_sort.go
@@ -0,0 +1,18 @@
+package insertion_sort
+
+func min[T any](array []T, start int, less func(a, b T) bool) int {
+	min := start
+	for i := start; i < len(array); i++ {
+		if less(array[i], array[min]) {
+			min = i
+		}
+	}
+	return min
+}
+
+func InsertionSort[T any](array []T, less func(a, b T) bool) {
+	for i := 0; i < len(array); i++ {
+		m := min(array, i, less)
+		array[i], array[m] = array[m], array[i]
+	}
+}
